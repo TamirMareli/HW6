5162b93c8354ea2698e88006275acde96d7bb120
TamirMareli <tqmirmareli4949@gmail.com>
2022-06-02 13:12:51 +0300

    finis

--- HW6/main.c
+++ HW6/main.c
@@ -3,21 +3,60 @@
 #include <stdlib.h>
 
 int pack4Chars2Int(char c1, char c2, char c3, char c4);
+int checkEvenParity(short input);
 
 void main()
 {
-    pack4Chars2Int('a', 'b', 'c', 'd');
+   int num = pack4Chars2Int('a', 'b', 'c', 'd');
+   int mask1 = 1;
+   mask1 <<= 31;
+   while (mask1 != 0) {
+       if ((mask1 & num)== mask1)
+           printf("1");
+       else
+           printf("0");
+       mask1 >>= 1;
+   }
+    short input = -14580;
+    printf( "\n%d", checkEvenParity(input));
 }
 
 int pack4Chars2Int(char c1, char c2, char c3, char c4)
 {
-    unsigned char sum=0;
-    sum += c1 | c2 | c3 | c4;
-    int  mask = 1;
-    while (mask > 0) {
-        sum += c1 | c2 | c3 | c4;
-        mask <<= 1;
-    }
+    int sum=0;
+    int temp = 0;
+    temp |= c1;
+    temp <<= 24;
+    sum = temp;
+    temp = 0;
 
-    printf("%u", sum);
+    temp |= c2;
+    temp<<= 16;
+    sum|=temp;
+    temp = 0;
+
+    temp |= c3;
+    temp <<= 8;
+    sum |= temp;
+    temp = 0;
+
+    sum |= c4;
+    
+    return sum;
+}
+
+int checkEvenParity(short input) {
+    int sum = 0;
+    short mask = 1;
+    while (mask != 0) 
+    {
+        if ((input & mask) ==mask)
+            sum++;
+
+        mask<<= 1;
+    }
+    if (sum % 2 == 0)
+        return 1;
+    return 0;
+
 }
